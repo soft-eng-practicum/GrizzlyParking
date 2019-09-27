26a0f959cc1a272bb7d2f415fb06530e22d93947
Josh Walton <jwalton@12stone.com>
2019-09-27 09:18:29 -0400

    Empty Branch

--- src/shared/models/user.ts
+++
@@ -1,6 +0,0 @@
-// INTERFACE  |  USER
-// Accessible to all pages via import - import { User } from "../../shared/models/user";
-export interface User {
-    email: string;
-    password: string;
-}

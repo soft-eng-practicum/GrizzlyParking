5092ee0b3d14a8aa6d0262a0071e29b46de18b6c
Mark Levengood <mlevengood@ggc.edu>
2019-10-29 17:35:02 -0400

    Added Parking lots I and F, and the Parking Deck with Scrollbar.

--- src/authentication/Profile.js
+++ src/authentication/Profile.js
@@ -1,10 +1,13 @@
 
 import { observer } from 'mobx-react';
 import React from 'react';
-import { Button, Text, View, StyleSheet, Image, Dimensions, ImageBackground } from 'react-native';
+import { Button, Text, View, StyleSheet, Image, Dimensions, ImageBackground, ScrollView, } from 'react-native';
 import A from '../../lib/images/L_Lot.jpg'
 import B from '../../lib/images/H_Lot.jpg'
 import C from '../../lib/images/A_Lot2.jpg'
+import D from '../../lib/images/I_Lot.jpg'
+import E from '../../lib/images/F_Lot.jpg'
+import F from '../../lib/images/Parking_Deck.jpg'
 
 const { width: WIDTH } = Dimensions.get('window')
 const { height: HEIGHT } = Dimensions.get('window')
@@ -14,36 +17,72 @@ export const Profile = observer(
         return (
 
             <View style={styles.background}>
-                <Text style={styles.logoText}> Parking Lots </Text>
+                <ScrollView style={styles.scrollView}>
 
-                <View>
-                    <Text style={styles.lotsText}>Lot A</Text>
-                </View>
-                <View>
-                    <Image source={C} style={styles.backgroundImage} />
-                </View>
+                  <Text style={styles.logoText}> Parking Lots </Text>
 
 
-                <View>
-                    <Text style={styles.lotsText}>Lot H</Text>
-                </View>
-                <View>
-                    <Image source={B} style={styles.backgroundImage} />
-                </View>
+
+                    <View>
+                        <Text style={styles.lotsText}>Lot H</Text>
+                    </View>
+                    <View>
+                        <Image source={B} style={styles.backgroundImage} />
+                    </View>
+
+
+
+                    <View>
+                        <Text style={styles.lotsText}>Parking Deck</Text>
+                    </View>
+                    <View>
+                        <Image source={F} style={styles.backgroundImage} />
+                    </View>
+
+
+
+                    <View>
+                        <Text style={styles.lotsText}>Lot F</Text>
+                    </View>
+                    <View>
+                        <Image source={E} style={styles.backgroundImage} />
+                    </View>
+
+
+
+                    <View>
+                        <Text style={styles.lotsText}>Lot I</Text>
+                    </View>
+                    <View>
+                        <Image source={D} style={styles.backgroundImage} />
+                    </View>
+
+
+
+                    <View>
+                        <Text style={styles.lotsText}>Lot A</Text>
+                    </View>
+                    <View>
+                        <Image source={C} style={styles.backgroundImage} />
+                    </View>
+
+
 
                 
-                <View>
-                    <Text style={styles.lotsText}>Lot L</Text>
-                </View>
+                    <View>
+                        <Text style={styles.lotsText}>Lot L</Text>
+                    </View>
 
-                <View>
-                    <Image source={A} style={styles.backgroundImage} />
-                </View>
+                    <View>
+                        <Image source={A} style={styles.backgroundImage} />
+                    </View>
 
-                <View style={styles.button}>
-                    <Button title="Go Back" color="#0A5A45"
-                        onPress={() => navigation.goBack()} />
-                </View>
+                    <View style={styles.button}>
+                        <Button title="Go Back" color="#0A5A45"
+                            onPress={() => navigation.goBack()} />
+                    </View>
+                
+                </ScrollView>
             </View>
         );
     })
@@ -59,7 +98,7 @@ const styles = StyleSheet.create({
     backgroundImage: {
         resizeMode: "contain",
         alignSelf: 'center',
-        width: 373,
+        width: 372,
         height: 177,
     },
 
